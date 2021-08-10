a4c832f1196a02bc447ef1eacc18da3d53b79fe8
Lorenzo Lopez <geltbm@gmail.com>
2021-08-10 12:45:23 -0700

    Made a program to simulate a buddy allocator with a visualizer

---
+++ 12_BuddyAllocator/12_BuddyAllocator/BuddyMemory.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_BuddyAllocator
+{
+    class BuddyMemory
+    {
+        byte[] arr;
+
+        public byte this[int i]
+        {
+            get => arr[i];
+            set => arr[i] = value;
+        }
+
+        public readonly int Size;
+        public readonly int MaxLevel;
+        public readonly int Unit;
+
+        public BuddyMemory()
+        {
+            Unit = 2;
+            MaxLevel = 6;
+            Size = Unit << MaxLevel;
+            arr = new byte[Size];
+            arr[0] = (byte)(MaxLevel << 1);
+        }
+
+        public bool TryAllocate(int bytes, out int address)
+        {
+            bytes++; //Add header size
+
+            byte newOrder = 0;
+            int newSize = Unit;
+            while(newSize < bytes)
+            {
+                newOrder++;
+                newSize *= 2;
+            }
+
+            int currentAddr = 0;
+            while (currentAddr < arr.Length)
+            {
+                int blockOrder = arr[currentAddr] >> 1;
+                bool isOccupied = (arr[currentAddr] & 1) == 1;
+
+                if(isOccupied)
+                {
+                    if(blockOrder <= newOrder)
+                    {
+                        currentAddr += Unit << newOrder;
+                    }
+                    else
+                    {
+                        currentAddr += Unit << blockOrder;
+                    }
+                }
+                else
+                {
+                    if (blockOrder < newOrder)
+                    {
+                        currentAddr += Unit << newOrder;
+                    }
+                    else if (blockOrder == newOrder)
+                    {
+                        arr[currentAddr] |= 1;
+                        address = currentAddr + 1;
+                        return true;
+                    }
+                    else
+                    {
+                        while(blockOrder > newOrder)
+                        {
+                            //Split
+                            blockOrder--;
+                            int buddyAddr = currentAddr ^ (Unit << blockOrder);
+                            arr[buddyAddr] = (byte)(blockOrder << 1);
+                        }
+
+                        arr[currentAddr] = (byte)((blockOrder << 1) | 1);
+                        address = currentAddr + 1;
+                        return true;
+                    }
+                }
+            }
+            address = 0;
+            return false;
+        }
+
+        public void Deallocate(int address)
+        {
+            address--;
+            int order = arr[address] >> 1;
+
+            while (order < MaxLevel)
+            {
+                int buddyAddr = address ^ (Unit << order);
+                if ((arr[buddyAddr] & 1) == 0)
+                {
+                    order++;
+                    if (buddyAddr < address)
+                    {
+                        address = buddyAddr;
+                    }
+                }
+                else break;
+            }
+            arr[address] = (byte)(order << 1);
+        }
+
+        public byte[] GetCopy()
+        {
+            byte[] ret = new byte[Size];
+            for(int i = 0; i < Size; i++)
+            {
+                ret[i] = arr[i];
+            }
+            return ret;
+        }
+    }
+}
---
+++ 12_BuddyAllocator/12_BuddyAllocator/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _12_BuddyAllocator
+{
+    class Program
+    {
+        static void DrawMemory(byte[] memory, int minBlockSize, int y)
+        {
+            int i = 0;
+            Console.SetCursorPosition(0, y);
+            while (i < memory.Length)
+            {
+                int blockOrder = memory[i] >> 1;
+                bool occupied = (memory[i] & 1) == 1;
+
+                Console.ForegroundColor = occupied ? (ConsoleColor)(blockOrder + 1) : ConsoleColor.White;
+
+                int orderWidth = minBlockSize << blockOrder;
+                Console.Write('|');
+                Console.Write(new string('-', orderWidth - 1));
+
+                i += orderWidth;
+            }
+            Console.WriteLine();
+        }
+
+        static void DrawMemoryMemory(IList<byte[]> memMem)
+        {
+            for (int i = 0; i < memMem.Count; i++)
+            {
+                DrawMemory(memMem[i], 2, i);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            BuddyMemory memory = new BuddyMemory();
+            Console.SetWindowSize(memory.Size + 2, 40);
+
+            List<byte[]> memoryMemory = new()
+            {
+                memory.GetCopy()
+            };
+
+            Console.CursorTop = 21;
+            while (true)
+            {
+                int savePos = Console.CursorTop;
+                DrawMemoryMemory(memoryMemory);
+
+                Console.SetCursorPosition(0, savePos);
+                Console.ForegroundColor = ConsoleColor.White;
+                string line = Console.ReadLine();
+                string[] parts = line.Split(' ');
+
+                bool save = true;
+                switch (parts[0].ToLower())
+                {
+                    case "alloc":
+                        if (parts.Length > 0 && int.TryParse(parts[1], out int allocBytes))
+                        {
+                            if(memory.TryAllocate(allocBytes, out int address))
+                            {
+                                Console.ForegroundColor = ConsoleColor.Cyan;
+                                Console.WriteLine(address);
+                            }
+                        }
+                        else save = false;
+                        break;
+                    case "free":
+                        if (parts.Length > 0 && int.TryParse(parts[1], out int freeAddr))
+                        {
+                            memory.Deallocate(freeAddr);
+                        }
+                        else save = false;
+                        break;
+                    default:
+                        save = false;
+                        continue;
+                }
+
+                if (save)
+                {
+                    memoryMemory.Add(memory.GetCopy());
+                    if (memoryMemory.Count > 20)
+                    {
+                        memoryMemory.RemoveAt(0);
+                    }
+                }
+            }
+        }
+    }
+}

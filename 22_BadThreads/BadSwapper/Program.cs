38416b6924c9d683828afff2bc0b82df550cfaa7
Lorenzo Lopez <geltbm@gmail.com>
2021-08-16 20:46:45 -0700

    Made some programs to demonstrate thread problems

---
+++ 22_BadThreads/BadSwapper/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+
+namespace BadSwapper
+{
+    struct SwapItem
+    {
+        long A;
+        long B;
+        public SwapItem(long A, long B)
+        {
+            this.A = A;
+            this.B = B;
+        }
+        public static bool operator ==(SwapItem a, SwapItem b)
+        {
+            return a.A == b.A && a.B == b.B;
+        }
+        public static bool operator !=(SwapItem a, SwapItem b)
+        {
+            return !(a == b);
+        }
+    }
+
+    class Program
+    {
+        static SwapItem ItemA = new SwapItem(1, 2);
+        static SwapItem ItemB = new SwapItem(3, 4);
+        static void Swap()
+        {
+            while (true)
+            {
+                var temp = ItemA;
+                ItemA = ItemB;
+                ItemB = temp;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Thread swapThread = new Thread(Swap);
+
+            SwapItem originalA = ItemA;
+            SwapItem originalB = ItemB;
+            swapThread.Start();
+
+            while(true)
+            {
+                if ( (ItemA != originalA && ItemA != originalB)||
+                    (ItemB != originalA && ItemB != originalB))
+                {
+                    ;
+                }
+            }
+        }
+    }
+}
---
+++ 22_BadThreads/BadThreads/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BadThreads
+{
+    class Program
+    {
+        private static int Number = 0;
+        static void Increment()
+        {
+            while (true)
+            {
+                Number++;
+            }
+        }
+        static void Decrement()
+        {
+            while (true)
+            {
+                Number--;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(new ThreadStart(Increment));
+            Thread t2 = new Thread(new ThreadStart(Decrement));
+
+            t1.Start();
+            t2.Start();
+
+            while (true)
+            {
+                Console.WriteLine(Number);
+            }
+        }
+    }
+}

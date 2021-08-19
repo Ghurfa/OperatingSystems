f7a9ed04ca970bb3839831776ab74bfd1444bc5f
Lorenzo Lopez <geltbm@gmail.com>
2021-08-18 20:44:25 -0700

    Made a multi thread counter to demonstrate lock overhead. Started on a concurrent linked list

---
+++ 23_ConcurrentLinkedList/23_ConcurrentLinkedList/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Threading;
+
+namespace _23_ConcurrentLinkedList
+{
+    class Node
+    {
+        public Node Next;
+        public Node(Node next)
+        {
+            Next = next;
+        }
+        public Node()
+        {
+            Next = null;
+        }
+    }
+
+    class NormalLinkedList
+    {
+        public int Count { get; private set; }
+        private Node Head;
+
+        public NormalLinkedList()
+        {
+            Head = null;
+            Count = 0;
+        }
+
+        public void Insert()
+        {
+            Head = new Node(Head);
+            Count++;
+        }
+
+        public int TraverseCount()
+        {
+            Node current = Head;
+            int count = 0;
+            while (current != null)
+            {
+                current = current.Next;
+                count++;
+            }
+            return count;
+        }
+    }
+
+    class ConcurrentLinkedList
+    {
+        private object LockObj;
+
+        private Node Head;
+
+        public ConcurrentLinkedList()
+        {
+            LockObj = new object();
+        }
+
+        public void Insert()
+        {
+            Head = new Node(Head);
+        }
+
+        public int TraverseCount()
+        {
+            lock (LockObj)
+            {
+                Node current = Head;
+                int count = 0;
+                while (current != null)
+                {
+                    current = current.Next;
+                    count++;
+                }
+                return count;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void NormalThreadJob(NormalLinkedList ll, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                ll.Insert();
+            }
+        }
+
+        static void NormalInsertTest()
+        {
+            int threadCount = 10;
+            int perThreadInsert = 1_000_000;
+
+            NormalLinkedList ll = new();
+            Thread[] threads = new Thread[threadCount];
+
+            for (int i = 0; i < threadCount; i++)
+            {
+                threads[i] = new Thread(() => NormalThreadJob(ll, perThreadInsert));
+            }
+            foreach (Thread thread in threads)
+            {
+                thread.Start();
+            }
+            foreach (Thread thread in threads)
+            {
+                thread.Join();
+            }
+            Console.WriteLine("Instructed count:\t" + threadCount * perThreadInsert);
+            Console.WriteLine("Stored count:\t\t" + ll.Count);
+            Console.WriteLine("Actual count:\t\t" + ll.TraverseCount());
+        }
+
+        static void Main(string[] args)
+        {
+            NormalInsertTest();
+        }
+    }
+}
---
+++ 23_MultiThreadCounter/MultiThreadCounter/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace MultiThreadCounter
+{
+    class Counter
+    {
+        public int Num;
+        private object LockObj;
+
+        public Counter()
+        {
+            Num = 0;
+            LockObj = new object();
+        }
+
+        public void Increment()
+        {
+            lock (LockObj)
+            {
+                Num++;
+            }
+        }
+    }
+
+    class MultiThreadCounter
+    {
+        public int Goal { get; private set; }
+        public int Value { get; private set; }
+
+        private object LockObj;
+        private int[] LocalCount;
+
+        public MultiThreadCounter(int count)
+        {
+            LocalCount = new int[count];
+            LockObj = new object();
+        }
+
+        public void Increment(int id, int threshold)
+        {
+            LocalCount[id]++;
+            if(LocalCount[id] >= threshold)
+            {
+                lock(LockObj)
+                {
+                    Value += LocalCount[id];
+                }
+                LocalCount[id] = 0;
+            }
+        }
+
+        public void Flush(int id)
+        {
+            lock (LockObj)
+            {
+                Value += LocalCount[id];
+            }
+            LocalCount[id] = 0;
+        }
+    }
+
+    class Program
+    {
+        static volatile bool Started = false;
+
+        static void ThreadJob1(Counter counter, int goal)
+        {
+            while (!Started) ;
+            while (counter.Num < goal)
+            {
+                counter.Increment();
+            }
+        }
+
+        static void Counter1(int numThreads)
+        {
+            int perThreadSize = 50_000_000;
+            int goal = numThreads * perThreadSize;
+            Thread[] threads = new Thread[numThreads];
+            Counter counter = new Counter();
+
+            for (int i = 0; i < numThreads; i++)
+            {
+                threads[i] = new Thread(() => ThreadJob1(counter, goal));
+                threads[i].Start();
+            }
+            Stopwatch timer = Stopwatch.StartNew();
+            Started = true;
+            for (int i = 0; i < numThreads; i++)
+            {
+                threads[i].Join();
+            }
+            timer.Stop();
+
+            Console.WriteLine(numThreads + " threads:\t" + timer.ElapsedMilliseconds + "\tPer:\t" + timer.ElapsedMilliseconds/numThreads);
+        }
+
+        static void ThreadJob2(MultiThreadCounter counter, int id, int goal)
+        {
+            while (!Started) ;
+            while (counter.Value < goal)
+            {
+                counter.Increment(id, 100);
+            }
+            counter.Flush(id);
+        }
+
+        static void Counter2(int numThreads)
+        {
+            int perThreadSize = 50_000_000;
+            int goal = numThreads * perThreadSize;
+            Thread[] threads = new Thread[numThreads];
+            MultiThreadCounter counter = new MultiThreadCounter(numThreads);
+
+            for (int i = 0; i < numThreads; i++)
+            {
+                int copy = i;
+                threads[i] = new Thread(() => ThreadJob2(counter, copy, goal));
+                threads[i].Start();
+            }
+            Stopwatch timer = Stopwatch.StartNew();
+            Started = true;
+            for (int i = 0; i < numThreads; i++)
+            {
+                threads[i].Join();
+            }
+            timer.Stop();
+
+            Console.WriteLine(numThreads + " threads:\t" + timer.ElapsedMilliseconds + "\tPer:\t" + timer.ElapsedMilliseconds / numThreads);
+        }
+
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Counter1(i);
+                Console.Write("\t\t\t\t\t\t");
+                Counter2(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}

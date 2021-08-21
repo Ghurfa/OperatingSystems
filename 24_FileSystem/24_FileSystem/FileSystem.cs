6b30d084675bc4dbed73d64fe809a16e7f48615c
Lorenzo Lopez <geltbm@gmail.com>
2021-08-20 21:09:46 -0700

    Worked on the file system exercise

--- 24_FileSystem/24_FileSystem/FileSystem.cs
+++ 24_FileSystem/24_FileSystem/FileSystem.cs
@@ -8,28 +8,99 @@ namespace _24_FileSystem
 {
     class FileSystem
     {
-        private byte[] Disk;
-
         public readonly int SectorSize;
+        public readonly int DescriptorSize;
+        public readonly int BlockHeaderSize;
+
+        private byte[] Disk;
+        private bool[] UsedBlocks;
 
         public FileSystem(byte[] disk)
         {
+            Disk = disk;
             SectorSize = 1024;
-            Disk = new byte[short.MaxValue];
+            DescriptorSize = 128;
+            BlockHeaderSize = sizeof(int);
+            UsedBlocks = new bool[disk.Length / SectorSize];
+            UsedBlocks[0] = true;
         }
 
-        public int GetFileID(string name)
+        private bool FileNameMatches(int addr, string name)
         {
-            for(int i = 0; i < 1024; i++)
+            int fileNameLen = Disk[addr];
+            if (fileNameLen == name.Length)
             {
-
+                for (int j = 0; j < fileNameLen; j++)
+                {
+                    char fileChar = (char)Disk[addr + 2 + j];
+                    if (fileChar != name[j]) return false;
+                }
+                return true;
             }
-            throw new NotImplementedException();
+            else return false;
         }
 
-        public void StoreFile(string name, Span<byte> data)
+        public bool TryGetFileID(string name, out int id)
         {
+            for (int i = 0; i < UsedBlocks.Length; i++)
+            {
+                int descAddr = i * DescriptorSize;
+                if (FileNameMatches(descAddr, name))
+                {
+                    id = i;
+                    return true;
+                }
+            }
+            id = 0;
+            return false;
+        }
 
+        public bool StoreFile(string name, Span<byte> data)
+        {
+            if (name.Length > 64) return false;
+
+            int descriptorPos = 0; //Todo
+            byte[] strBytes = name.ToCharArray().Select(x => Convert.ToByte(x)).ToArray();
+            Disk[descriptorPos] = (byte)name.Length;
+            strBytes.CopyTo(Disk, descriptorPos + 2);
+
+            int fileSize = data.Length;
+            BitConverter.GetBytes(fileSize).CopyTo(Disk, descriptorPos + 66);
+
+            if (TryAllocateBlock(out int addr))
+            {
+                BitConverter.GetBytes(addr).CopyTo(Disk, descriptorPos + 66 + sizeof(int));
+                return true;
+            }
+            else return false;
+        }
+
+        public IEnumerable<Memory<byte>> LoadFileBlock(string name)
+        {
+            if (!TryGetFileID(name, out int id)) throw new InvalidOperationException();
+
+            int descAddr = DescriptorSize * id;
+            int fileAddr = BitConverter.ToInt32(Disk, descAddr + 66 + sizeof(int));
+
+            while (fileAddr != 0)
+            {
+                yield return new Memory<byte>(Disk, fileAddr + BlockHeaderSize, SectorSize - BlockHeaderSize);
+                fileAddr = BitConverter.ToInt32(Disk, fileAddr);
+            }
+        }
+
+        private bool TryAllocateBlock(out int addr)
+        {
+            for (int i = 0; i < UsedBlocks.Length; i++)
+            {
+                if (!UsedBlocks[i])
+                {
+                    addr = i * SectorSize;
+                    return true;
+                }
+            }
+            addr = 0;
+            return false;
         }
     }
 }
--- 24_FileSystem/24_FileSystem/Program.cs
+++ 24_FileSystem/24_FileSystem/Program.cs
@@ -6,7 +6,11 @@ namespace _24_FileSystem
     {
         static void Main(string[] args)
         {
+            byte[] disk = new byte[short.MaxValue];
+            FileSystem fs = new FileSystem(disk);
 
+            byte[] file = {1, 5, 8, 3, 123};
+            fs.StoreFile("file1", file);
         }
     }
 }
